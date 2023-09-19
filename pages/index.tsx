c2855134b1e108485366f37ca2ba6d66cea72ac8
GDC94 <germanderbescatoni@gmail.com>
2023-09-19 15:17:38 -0300

    delete console.log

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,7 @@
-
-import { useWeb3 } from "context/web3Context/web3Context";
 import { MainLayout } from "layouts/mainLayout";
 import Head from "next/head";
 
 export default function Home() {
-  const {web3, methods} = useWeb3();
-  console.log(web3, methods?.connect);
-  
-
-  
   return (
     <MainLayout>
       <Head>
